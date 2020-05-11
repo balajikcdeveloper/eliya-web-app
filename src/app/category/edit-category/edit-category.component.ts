e810102f843633b2202483765342647a6773b21d
Sampath <sampathkumarr2000@gmail.com>
2020-05-11 12:13:13 +0530

    added icon selection service in edit category .

--- src/app/category/edit-category/edit-category.component.ts
+++ src/app/category/edit-category/edit-category.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit,Inject } from '@angular/core';
+import { Component, OnInit,Inject,EventEmitter, Output } from '@angular/core';
 import { HttpStatusCode } from '../../constents/http-status-code';
 import { NotificationService } from '../../shared/services/general/notification.service';
 import { CategoryService } from '../services/category.service';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { Category } from '../models/category-dto';
+import { CategoryIconComponent } from '../category-icon/category-icon.component';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,12 +14,16 @@ import { Category } from '../models/category-dto';
   styleUrls: ['./edit-category.component.scss']
 })
 export class EditCategoryComponent implements OnInit {
+
   categoryList:Category = new Category();
+  
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: {category:Category},
     private categoryService:CategoryService,
     private dialogRef: MatDialogRef<EditCategoryComponent>,
-    private notificationService:NotificationService
+    private notificationService:NotificationService,
+    private router : Router,
+    public dialog: MatDialog
   ) {
     this.categoryList = Object.assign({},data.category);
    }
@@ -33,4 +39,17 @@ export class EditCategoryComponent implements OnInit {
       }
     });
   }
+  updateIcon() {
+     const dialogRef = this.dialog.open(CategoryIconComponent, {
+      width: '50%',
+      
+      data: {},
+    });
+    dialogRef.afterClosed().subscribe((icon: any) => {
+      if (icon) {
+        this.categoryList.icon = icon;
+      }
+    });
 }
+}
+
